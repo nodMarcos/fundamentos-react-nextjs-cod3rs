a45777aca57d40eda388abe05cc80eecbc41c482
Marcos Felipe <nodmarcos@gmail.com>
2021-12-17 09:28:24 -0300

    project finished, all pages and components done

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,9 @@ export default function Inicio() {
             <Navegador destino='/jsx' color='crimson' text='JSX'/>
             <Navegador destino='/navegacao' color='green' text='Navegação #01'/>
             <Navegador destino='/cliente/sp-2/123' color='blue' text='Navegação #02'/>
+            <Navegador destino='/estado' color='#a45b71' text='Componente com Estado'/>
+            <Navegador destino='/integracao_1' color='#42a9a9' text='Integração com API #01'/>
+            <Navegador destino='/estatico' color='#fa054a' text='Conteúdo Estático'/>
 
         </div>
     )
